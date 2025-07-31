62f5c68d57a9c3c67c845b37564f498fa465d6e2
eslim <85449803+eslimrateb@users.noreply.github.com>
2025-07-31 15:13:06 +0300

    add brand list

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,10 @@
-from .views import ProductList, ProductDetail
+from .views import BrandList, ProductList, ProductDetail,BrandList
 from django.urls import path
 
 
 urlpatterns = [
     path("", ProductList.as_view()),
+    path("brands/", BrandList.as_view()),
     path("<slug:slug>/", ProductDetail.as_view()),
+    
 ]
--- product/views.py
+++ product/views.py
@@ -12,3 +12,6 @@ class ProductDetail(DetailView):
         context['reviews'] = Review.objects.filter(product=self.object)
         context['related_products'] = Product.objects.filter(brand=self.object.brand).exclude(id=self.object.id)
         return context
+    
+class BrandList(ListView):
+    model = Brand
